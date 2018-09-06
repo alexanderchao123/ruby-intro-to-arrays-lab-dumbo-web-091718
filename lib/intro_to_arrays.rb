387926da3313082be5ec414a3e3270f4098a03af
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-06 06:25:32 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,15 +7,15 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array[0]
+  return array[0]
 end
 
 def third_element(array)
-  array[2]
+  return array[2]
 end
 
 def last_element(array)
-  array[-1]
+  return array[-1]
 end
 
 def first_element_with_array_methods(array)
